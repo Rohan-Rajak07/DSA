38a2eb6682eed03845be0eada8d27409388a35f7
Rohan-Rajak07 <rohanrajak132@gmail.com>
2024-09-27 13:33:44 +0530

    Pattern

---
+++ _0Numbers/q9_PrintPattern.java
@@ -0,0 +1,36 @@
+package _0Numbers;
+import java.util.*;
+
+public class q9_PrintPattern 
+{
+    // 16 11 6 1 -4 1 6 11 16
+    public static void main(String[] args) 
+    {
+        ArrayList<Integer>l=new ArrayList<>();
+
+        int n=10;
+        l.add(n);
+
+        int a=n-5;
+        l.add(a);
+
+        int c=0;
+
+        while(a!=n)
+        {
+            if(a>0 && c==0)
+            {
+                a=a-5;
+                l.add(a);
+            }
+            else
+            {
+                a=a+5;
+                l.add(a);
+                c=1;
+            }
+        }
+        System.out.println(l);
+    }
+    
+}
