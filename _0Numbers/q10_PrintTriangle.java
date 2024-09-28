2cae77fc962109e35818ff7d700a76f88da28618
Rohan-Rajak07 <rohanrajak132@gmail.com>
2024-09-28 13:43:50 +0530

    solve pattern

---
+++ _0Numbers/q10_PrintTriangle.java
@@ -0,0 +1,17 @@
+package _0Numbers;
+
+public class q10_PrintTriangle 
+{
+    public static void main(String[] args) 
+    {
+        int n=5;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=1; j<=n-i; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
